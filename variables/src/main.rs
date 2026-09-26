50920a7d753c82f787cf9b8900cb1b257e87929c
Dmitry <mutasovdmitry@gmail.com>
2026-09-26 09:37:00 +0700

    let's get rusty

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+
+fn main() {
+
+    println!("i8:{}",i8::MAX);
+    println!("u8:{}",u8::MAX);
+    println!("i16:{}",i16::MAX);
+    println!("u16:{}",u16::MAX);
+    println!("i32:{}",i32::MAX);
+    println!("u32:{}",u32::MAX);
+    println!("i64:{}",i64::MAX);
+    println!("u64:{}",u64::MAX);
+    println!("i128:{}",i128::MAX);
+    println!("u128:{}",u128::MAX);
+
+}
